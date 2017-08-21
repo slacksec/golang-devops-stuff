5e0b473d316913bd7b0c67e84ec76450aecf1d81
Tao Huang <tao.huang@thlonline.com>
2017-08-22 11:07:14 +1200

    Squashed 'src/github.com/ccding/go-stun/' changes from 65db37962..04a4eed61
    04a4eed61 fix readme
    90d35fd26 fix icon for test
    f60cf1449 add OTHER-ADDRESS support per RFC5780
    dee5672f0 Merge pull request #19 from AudriusButkevicius/f2
    045403231 Merge pull request #18 from AudriusButkevicius/fixes
    3cc31ce4e Fix panic in keepalive
    9df189015 Fix two more bugs (fixes #17)
    353de72d3 delete empty line
    f70474325 add packet test
    e2da02df7 add util test
    8b1c9f3d3 family is 1 byte
    11dbe3a2b update packet/attribute api
    e4dfdc851 Merge pull request #16 from ccding/dev
    14893aeb6 check server error by resp address
    a3a2513e7 clean func apis
    40426479b clean discover logic
    9bfbd8df5 fix ip family bug in host
    438e92fba update main.go
    805dc931e clean code
    7b5d5e1ea fix mistake in log
    7bd1db609 fix changedAddr nil bug
    fecb06d7d clean discover()
    28d6e0a0c opt -v -vv
    3431f807d change log format
    4839d26b7 change log behavior
    ddcc63ab3 add response struct
    afe732546 refactor code
    bc56a69d9 id -> transId
    c5e4803fd Merge pull request #15 from ccding/dev
    bc8d773fa change default server
    c1faf0c6b check trans id in send and resp
    a2b8ce252 use camal case for const
    6558e5e48 Merge pull request #14 from AudriusButkevicius/ka
    5f01c49ba Add keepalive method
    a7d6c5073 fix test3 address issue
    773aa389a Merge pull request #13 from ccding/dev
    f6f2608d1 fix magic cookie issue
    c7381be3c add debug mode
    03fae0ad6 fix xoredMap issue
    6d490d9d6 clean code
    659cdbd1d changedAddr->changeAddr, : "->:"
    23b98eef7 output received from addr in verbose
    c27e697ed fix the second round test1 bug
    5d6db7bda fix typo
    f72d2aea0 add RFC link to readme
    1047806a4 add copyright for log
    f4cebeab9 fix golint issues
    d25dd95de fix comments
    f1f98c6c0 add comment for log
    8686b2477 update readme
    c61637209 update readme
    b07fe5974 use main to make it be standalone
    10f93926e add verbose mode
    50a1428b1 Merge pull request #11 from ccding/dev
    6fbb3bb4d clean set host process
    1dc66064e resove udp addr each time
    3bb3944e0 fix conn leakage issue
    e5f9c8414 use current version of go in travis
    56888d7f8 change license position
    0c3b5ced8 add license badge
    a4b771f6a check errors in generating packets
    90508e4ab fix
    cf07b6979 fix go lint issues
    6c7b257e4 add go report card
    6e645c897 change variable name
    1677a6446 avoid ResolveUDPAddr in second time discover
    ccfd63fa1 fix second time calling discover bug again
    b4fbbb184 fix bug in second time discover with the same client
    e13ae51db use google stun server by default
    43db56f1b check changed addr first then conn for test1
    4e00321f9 Merge pull request #10 from ccding/dev
    e657458d2 allow non-changed addr in test1 (fix #9)
    54567dc54 update example
    0fac5076d Merge pull request #8 from AudriusButkevicius/xor
    e8d4e6d1e Fix port XORing
    d8113ddef Merge pull request #7 from AudriusButkevicius/master
    30681dbd9 Fix multiple bugs
    1101be07d Merge pull request #6 from AudriusButkevicius/connection
    0142dc8ce Reuse the same connection for requests (fixes #5)
    df9c17720 Provide String() method for NATType
    ed7f5c90f update readme
    e32a5b703 fix
    6feda209b add comment for the discover process
    5310c941e clean discover process
    874d0e6c6 update const variables
    831a205b3 update doc for client
    6f2174eda return nil when err==nil
    a20f63e0b clean code
    fceb79eaf add client
    df1ca4c44 Ip -> IP
    e6fde177d clean code
    0ab6fde60 use NATType
    873d284e8 update comments
    9d767a265 update readme
    e0d59d62b clean code
    d59703999 s/author/Author/
    817a6d130 clean code and comments
    fbfdc0ad4 add build status
    7e89f3d22 add travis test file
    6cc0c24ec add test for discover
    a932cec52 update gitignore
    a4b010dd7 change stun server
    4e7a6055d Merge pull request #2 from DiscoViking/ipv4display
    0269f2ca3 Fix bug where sometimes IPv4 addresses get displayed as IPv6
    
    git-subtree-dir: src/github.com/ccding/go-stun
    git-subtree-split: 04a4eed61c57ecc9903f8983d1d2c17b88d2e9e1

--- stun/client.go
+++ stun/client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// author: Cong Ding <dinggnu@gmail.com>
+// Author: Cong Ding <dinggnu@gmail.com>
 
 package stun
 
@@ -22,46 +22,105 @@ import (
 	"strconv"
 )
 
-var (
+// Client is a STUN client, which can be set STUN server address and is used
+// to discover NAT type.
+type Client struct {
 	serverAddr   string
 	softwareName string
-)
-
-func init() {
-	SetSoftwareName(DefaultSoftwareName)
+	conn         net.PacketConn
+	logger       *Logger
 }
 
-// SetServerHost allows user to set the STUN hostname and port
-func SetServerHost(host string, port int) error {
-	ips, err := net.LookupHost(host)
-	if err != nil {
-		return err
-	}
-	if len(ips) == 0 {
-		return errors.New("Failed to get IP address of " + host)
-	}
-	serverAddr = net.JoinHostPort(ips[0], strconv.Itoa(port))
-	return nil
+// NewClient returns a client without network connection. The network
+// connection will be build when calling Discover function.
+func NewClient() *Client {
+	c := new(Client)
+	c.SetSoftwareName(DefaultSoftwareName)
+	c.logger = NewLogger()
+	return c
 }
 
-// SetServerAddr allows user to set the transport layer STUN server address
-func SetServerAddr(address string) {
-	serverAddr = address
+// NewClientWithConnection returns a client which uses the given connection.
+// Please note the connection should be acquired via net.Listen* method.
+func NewClientWithConnection(conn net.PacketConn) *Client {
+	c := new(Client)
+	c.conn = conn
+	c.SetSoftwareName(DefaultSoftwareName)
+	c.logger = NewLogger()
+	return c
 }
 
-// SetSoftwareName allows user to set the name of her software
-func SetSoftwareName(name string) {
-	softwareName = name
+// SetVerbose sets the client to be in the verbose mode, which prints
+// information in the discover process.
+func (c *Client) SetVerbose(v bool) {
+	c.logger.SetDebug(v)
+}
+
+// SetVVerbose sets the client to be in the double verbose mode, which prints
+// information and packet in the discover process.
+func (c *Client) SetVVerbose(v bool) {
+	c.logger.SetInfo(v)
+}
+
+// SetServerHost allows user to set the STUN hostname and port.
+func (c *Client) SetServerHost(host string, port int) {
+	c.serverAddr = net.JoinHostPort(host, strconv.Itoa(port))
+}
+
+// SetServerAddr allows user to set the transport layer STUN server address.
+func (c *Client) SetServerAddr(address string) {
+	c.serverAddr = address
+}
+
+// SetSoftwareName allows user to set the name of the software, which is used
+// for logging purpose (NOT used in the current implementation).
+func (c *Client) SetSoftwareName(name string) {
+	c.softwareName = name
 }
 
 // Discover contacts the STUN server and gets the response of NAT type, host
-// for UDP punching
-func Discover() (int, *Host, error) {
-	if serverAddr == "" {
-		err := SetServerHost(DefaultServerHost, DefaultServerPort)
-		if err != nil {
-			return NAT_ERROR, nil, err
-		}
+// for UDP punching.
+func (c *Client) Discover() (NATType, *Host, error) {
+	if c.serverAddr == "" {
+		c.SetServerAddr(DefaultServerAddr)
 	}
-	return discover()
+	serverUDPAddr, err := net.ResolveUDPAddr("udp", c.serverAddr)
+	if err != nil {
+		return NATError, nil, err
+	}
+	// Use the connection passed to the client if it is not nil, otherwise
+	// create a connection and close it at the end.
+	conn := c.conn
+	if conn == nil {
+		conn, err = net.ListenUDP("udp", nil)
+		if err != nil {
+			return NATError, nil, err
+		}
+		defer conn.Close()
+	}
+	return c.discover(conn, serverUDPAddr)
+}
+
+// Keepalive sends and receives a bind request, which ensures the mapping stays open
+// Only applicable when client was created with a connection.
+func (c *Client) Keepalive() (*Host, error) {
+	if c.conn == nil {
+		return nil, errors.New("no connection available")
+	}
+	if c.serverAddr == "" {
+		c.SetServerAddr(DefaultServerAddr)
+	}
+	serverUDPAddr, err := net.ResolveUDPAddr("udp", c.serverAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.test1(c.conn, serverUDPAddr)
+	if err != nil {
+		return nil, err
+	}
+	if resp == nil || resp.packet == nil {
+		return nil, errors.New("failed to contact")
+	}
+	return resp.mappedAddr, nil
 }
