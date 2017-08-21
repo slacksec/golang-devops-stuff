4f3125aaa34af7034673991b40c3e79b751c4efd
Tao Huang <tao.huang@thlonline.com>
2017-08-22 11:12:19 +1200

    Squashed 'src/github.com/jyap808/jaeger/' changes from 5cb3107b36..7d86b3d48e
    7d86b3d48e Remove debugging line
    b79be2e24a Look for an existing template file when initializing a JSON GPG database file
    4976c4f1d4 code.google.com is deprecated
    e905005b9b fix template output tags
    dea87ee078 Add output lines for templates
    9a8d68938b Remove extra calls after log.Fatalf since it exits anyway
    dce81a4f8a add checkExistsJaegerT function
    9ceae8ecd7 ignore lines with comments
    7dbfdb429b Disregard empty values
    ef9722b30e Add jaegerh. Jaeger Helper
    
    git-subtree-dir: src/github.com/jyap808/jaeger
    git-subtree-split: 7d86b3d48e6b37a6840a3e34f7a60bb8c3b8d9eb

---
+++ jaegerh/jaegerh.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+const jaegerTemplateExtension = ".jgrt"
+const jaegerDescription = "JaegerH - Jaeger Helper program\n\nJaeger is a JSON encoded GPG encrypted key value store. It is useful for separating development with operations and keeping configuration files secure."
+const jaegerQuote = "\"Stacker Pentecost: Haven't you heard Mr. Beckett? The world is coming to an end. So where would you rather die? Here? Or in a Jaeger!\" - Pacific Rim"
+const jaegerRecommendedUsage = "RECOMMENDED:\n    jaegerh -i file.txt.jgrt"
+
+var debug debugging = false
+
+type debugging bool
+
+func (d debugging) Printf(format string, args ...interface{}) {
+	// From: https://groups.google.com/forum/#!msg/golang-nuts/gU7oQGoCkmg/BNIl-TqB-4wJ
+	if d {
+		log.Printf(format, args...)
+	}
+}
+
+func main() {
+	// Define flags
+	var (
+		debugFlag     = flag.Bool("d", false, "Enable Debug")
+		inputTemplate = flag.String("i", "", "Input Template file. eg. file.txt.jgrt")
+	)
+
+	flag.Usage = func() {
+		fmt.Printf("%s\n%s\n\n%s\n\n", jaegerDescription, jaegerQuote, jaegerRecommendedUsage)
+		fmt.Fprintf(os.Stderr, "OPTIONS:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *debugFlag {
+		debug = true
+	}
+
+	if *inputTemplate == "" {
+		assumedTemplate, err := checkExistsJaegerT()
+		if err != nil {
+			flag.Usage()
+			log.Fatalf("\n\nError: %s", err)
+		}
+		*inputTemplate = assumedTemplate
+	}
+
+	processInputFile(inputTemplate)
+}
+
+func checkExistsJaegerT() (string, error) {
+	// Check that one template file is in the current directory and use that file
+	files, err := filepath.Glob("*.jgrt")
+	if err != nil {
+		return "", err
+	}
+	if len(files) == 1 {
+		return files[0], nil
+	}
+	return "", fmt.Errorf("No input template file specified")
+}
+
+func processInputFile(inputFile *string) {
+	file, err := os.Open(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parseString := scanner.Text()
+		matched, _ := regexp.MatchString("^[[:space:]]*#", parseString)
+		if matched {
+			if debug {
+				fmt.Println("Ignoring: %s", parseString)
+			}
+			continue
+		}
+		matched, _ = regexp.MatchString(".*=.*", parseString)
+		if matched {
+			parseLine(parseString)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseLine(line string) {
+	s := regexp.MustCompile("=").Split(line, 2)
+	rawKey := strings.TrimSpace(s[0])
+	key := camelKey(rawKey)
+	value := strings.TrimSpace(s[1])
+	if value != "" {
+		fmt.Printf("jaegerdb -a %s -v '%s' # %s = {{.%s}}\n", key, value, rawKey, key)
+	}
+}
+
+func camelKey(src string) string {
+	// From: https://github.com/etgryphon/stringUp
+
+	var camelingRegex = regexp.MustCompile("[0-9A-Za-z]+")
+
+	byteSrc := []byte(src)
+	chunks := camelingRegex.FindAll(byteSrc, -1)
+	for idx, val := range chunks {
+		chunks[idx] = bytes.Title(val)
+	}
+	return string(bytes.Join(chunks, nil))
+}
